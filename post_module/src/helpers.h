feabe9dba8adb37df109ab762b95afecf8c50dea
Mustafa Bhotvawala <mustafa.bhotvawala@ansys.com>
2024-01-19 12:10:34 +0530

    formats

--- post_module/src/helpers.h
+++ post_module/src/helpers.h
@@ -8,90 +8,132 @@
 
 #include <vector>
 
-#include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
 
 using omp_iterator = long long;
 
 namespace py = pybind11;
 
-template<typename T>
-class Span {
+template <typename T>
+class Span
+{
 public:
-    using iterator = T*;
-    using const_iterator = const T*;
+	using iterator = T*;
+	using const_iterator = const T*;
 
-    using reference = T&;
-    using const_reference = const T&;
+	using reference = T&;
+	using const_reference = const T&;
 
-    using pointer = T*;
-    using const_pointer = const T*;
+	using pointer = T*;
+	using const_pointer = const T*;
 
-    using value_type = T;
+	using value_type = T;
 
-    Span() = default;
-    Span(const Span<T>&) = default;
-    Span(Span<T>&&) = default;
-    ~Span() = default;
-    Span<T>& operator=(Span<T>&&) = default;
-    Span<T>& operator=(const Span<T>&) = default;
+	Span() = default;
+	Span(const Span<T>&) = default;
+	Span(Span<T>&&) = default;
+	~Span() = default;
+	Span<T>& operator=(Span<T>&&) = default;
+	Span<T>& operator=(const Span<T>&) = default;
 
-    template <typename U = T, typename = typename std::enable_if<std::is_const_v<U>>>
-    Span(const Span<std::remove_const_t<T>>& s) noexcept : sz_(s.size()), mem_(s.data()) {}
-    Span(const size_t n, T* data) noexcept : sz_(n), mem_(data) {}
+	template <typename U = T, typename = typename std::enable_if<std::is_const_v<U>>>
+	Span(const Span<std::remove_const_t<T>>& s) noexcept
+		: sz_(s.size())
+		, mem_(s.data())
+	{}
+	Span(const size_t n, T* data) noexcept
+		: sz_(n)
+		, mem_(data)
+	{}
 
-    Span(const std::vector<std::remove_const_t<T>>& vec) noexcept : sz_(vec.size()), mem_(vec.data()) {}
+	Span(const std::vector<std::remove_const_t<T>>& vec) noexcept
+		: sz_(vec.size())
+		, mem_(vec.data())
+	{}
 
-    Span(std::vector<T>& vec) noexcept : sz_(vec.size()), mem_(vec.data()) {}
+	Span(std::vector<T>& vec) noexcept
+		: sz_(vec.size())
+		, mem_(vec.data())
+	{}
 
-    operator std::vector<std::remove_const_t<T>>() const { return std::vector<std::remove_const_t<T>>(begin(), end()); }
+	operator std::vector<std::remove_const_t<T>>() const
+	{
+		return std::vector<std::remove_const_t<T>>(begin(), end());
+	}
 
-    iterator begin() const { return mem_; }
-    iterator end() const { return mem_ + sz_; }
+	iterator begin() const
+	{
+		return mem_;
+	}
+	iterator end() const
+	{
+		return mem_ + sz_;
+	}
 
-    pointer data() const { return mem_; }
+	pointer data() const
+	{
+		return mem_;
+	}
 
-    bool empty() const { return sz_ == 0; }
-    size_t size() const { return sz_; }
+	bool empty() const
+	{
+		return sz_ == 0;
+	}
+	size_t size() const
+	{
+		return sz_;
+	}
 
-    reference operator[](const size_t idx) const { return mem_[idx]; }
+	reference operator[](const size_t idx) const
+	{
+		return mem_[idx];
+	}
 
 private:
-    size_t sz_ = 0;
-    T* mem_ = nullptr;
+	size_t sz_ = 0;
+	T* mem_ = nullptr;
 };
 
 template <typename T>
-pybind11::array CastVectorToNumpy(std::vector<T> cont) {
-    pybind11::buffer_info buf;
-    buf.format = pybind11::format_descriptor<T>::format();
-    buf.itemsize = sizeof(T);
-    buf.ndim = 1;
-    buf.shape = { (pybind11::ssize_t)cont.size() };
-    buf.strides = { (pybind11::ssize_t)sizeof(T) };
-    std::vector<T>* mptr = new std::vector<T>(std::move(cont));
-    pybind11::capsule free_when_done(mptr, [](void* ptr) {
-        std::vector<T>* pmem = reinterpret_cast<std::vector<T>*>(ptr);
-    delete pmem;
-        });
-    buf.ptr = mptr->data();
-    return pybind11::array(buf, free_when_done);
+pybind11::array CastVectorToNumpy(std::vector<T> cont)
+{
+	pybind11::buffer_info buf;
+	buf.format = pybind11::format_descriptor<T>::format();
+	buf.itemsize = sizeof(T);
+	buf.ndim = 1;
+	buf.shape = {(pybind11::ssize_t)cont.size()};
+	buf.strides = {(pybind11::ssize_t)sizeof(T)};
+	std::vector<T>* mptr = new std::vector<T>(std::move(cont));
+	pybind11::capsule free_when_done(mptr, [](void* ptr) {
+		std::vector<T>* pmem = reinterpret_cast<std::vector<T>*>(ptr);
+		delete pmem;
+	});
+	buf.ptr = mptr->data();
+	return pybind11::array(buf, free_when_done);
 }
 
 template <typename T>
-Span<T> CastNumpyToSpan(const pybind11::array& obj) {
-    const pybind11::buffer_info buf = obj.request();
-    if (buf.shape[0] > 0) {
-        const size_t buf_ndim = static_cast<size_t>(buf.ndim);
-        if (buf.itemsize != sizeof(T)) {
-            throw std::invalid_argument("Wrong data type! Array type expeceted to be " + std::to_string(sizeof(T)) + " bytes. Type found has " + std::to_string(buf.itemsize) + " bytes");
-        }
-        if (buf_ndim != 1) {
-            throw std::invalid_argument("Number of dimensions is not 1 for a scalar type");
-        }
-        if (buf.strides[0] != sizeof(T)) {
-            throw std::invalid_argument("Wrong stride. Given scalar array is not C-contiguous");
-        }
-    }
-    return Span<T>(buf.shape[0], reinterpret_cast<T*>(buf.ptr));
+Span<T> CastNumpyToSpan(const pybind11::array& obj)
+{
+	const pybind11::buffer_info buf = obj.request();
+	if(buf.shape[0] > 0)
+	{
+		const size_t buf_ndim = static_cast<size_t>(buf.ndim);
+		if(buf.itemsize != sizeof(T))
+		{
+			throw std::invalid_argument("Wrong data type! Array type expeceted to be " +
+										std::to_string(sizeof(T)) + " bytes. Type found has " +
+										std::to_string(buf.itemsize) + " bytes");
+		}
+		if(buf_ndim != 1)
+		{
+			throw std::invalid_argument("Number of dimensions is not 1 for a scalar type");
+		}
+		if(buf.strides[0] != sizeof(T))
+		{
+			throw std::invalid_argument("Wrong stride. Given scalar array is not C-contiguous");
+		}
+	}
+	return Span<T>(buf.shape[0], reinterpret_cast<T*>(buf.ptr));
 }
--- post_module/src/post.cpp
+++ post_module/src/post.cpp
@@ -16,27 +16,33 @@
 
 namespace py = pybind11;
 
-void modify_array(pybind11::array& a_py) {
+void modify_array(pybind11::array& a_py)
+{
 	Span<double> a = CastNumpyToSpan<double>(a_py);
 #pragma omp parallel for schedule(static) num_threads(omp_get_max_threads())
-	for (omp_iterator i = 0; i < a.size(); i++) {
+	for(omp_iterator i = 0; i < a.size(); i++)
+	{
 		a[i] *= std::sin(a[i]);
 		a[i] *= std::cos(a[i]);
 	}
 }
 
-pybind11::array make_new_array(const pybind11::array& a_py, const double factor) {
+pybind11::array make_new_array(const pybind11::array& a_py, const double factor)
+{
 	const Span<const double> a = CastNumpyToSpan<const double>(a_py);
 	std::vector<double> b(a.size());
 #pragma omp parallel for schedule(static) num_threads(omp_get_max_threads())
-	for (omp_iterator i = 0; i < b.size(); i++) b[i] = a[i] * factor;
+	for(omp_iterator i = 0; i < b.size(); i++)
+		b[i] = a[i] * factor;
 	return CastVectorToNumpy(b);
 }
 
-pybind11::dict create_data() {
+pybind11::dict create_data()
+{
 	std::vector<double> a(10 * 1000);
 #pragma omp parallel for schedule(static) num_threads(omp_get_max_threads())
-	for (omp_iterator i = 0; i < a.size(); i++) a[i] = i % 9;
+	for(omp_iterator i = 0; i < a.size(); i++)
+		a[i] = i % 9;
 	double b = 3.0 - 0.0;
 	size_t c = std::numeric_limits<size_t>::max() + 0;
 
@@ -47,7 +53,8 @@ pybind11::dict create_data() {
 	return output;
 }
 
-PYBIND11_MODULE(post_module, m) {
+PYBIND11_MODULE(post_module, m)
+{
 	m.def("add", [](int i, int j) { return i + j; });
 	m.def("modify_array", &modify_array);
 	m.def("make_new_array", &make_new_array);
