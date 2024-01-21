3c56d0ba1f37168dffd70acda8fe2e96b7c32a03
Mustafa Bhotvawala <mustafa.bhotvawala@ansys.com>
2024-01-21 16:04:30 +0530

    changes

--- main.py
+++ main.py
@@ -1,41 +1,71 @@
 import numpy as np
-
+import pyvista as pv
 import nemo as n
-
 import post_module as pm
-
-# mesh: n.Mesh = n.cylinder(radius=1, height=1, axis=n.Vector(0, 1, 0))
-# print("")
-# print("mesh connectivity:")
-# print(mesh.data.segments[:3, :])
-# print("...")
-# print(mesh.data.segments[-3:, :])
-
-
-# print("")
-# print("add:")
-# print(pm.add(1, 3))
-# print("")
-
-# print("modify_array:")
-# a = np.arange(100_000_000).astype(np.float64)
-# print(a)
-# pm.modify_array(a)
-# print(a)
-# print("")
-
-# print("make_new_array:")
-# b = pm.make_new_array(a, 4.0)
-# print(b)
-# print("")
-
-# print("create_data:")
-# for k, v in pm.create_data().items():
-#     print(k, v, type(v))
-# print("")
 
 
 pointcloud = n.Particles.load(n.File("D:\\Mustafa\\Simulations\\short_simulation\\fluid_6800.pvtp")).data
-print(pointcloud.points)
-print(pointcloud.attributes["density"] )
+# print(pointcloud.points)
+# print(pointcloud.attributes["density"] )
 
+def visualize_particles():
+    density = pointcloud.attributes["density"]
+    pdata = pv.PolyData(pointcloud.points)
+    sphere = pv.Sphere(radius=0.02, phi_resolution=10, theta_resolution=10)
+    pc = pdata.glyph(scale=False, geom=sphere, orient=False, rng=[np.min(density), np.max(density)])
+    pc.plot()
+
+
+def write_to_stl():
+    x_resolution = 10
+    y_resolution = 10
+
+    x = np.linspace(0.07, 0.17, x_resolution)
+    y = np.linspace(0.07, 0.17, y_resolution)
+    z = np.zeros(1)  # 2D box, so z dimension has only one value
+
+    grid = pv.RectilinearGrid()
+    grid.x = x
+    grid.y = y
+    grid.z = z
+
+    grid.plot("show_edges=True")
+    mesh = grid.cast_to_structured_grid()
+    mesh = mesh.extract_surface()
+    mesh.save("output.stl")
+
+def read_into_nemo(): #note I could only read back an STL, so it's going to be triangulated..
+     mesh_display = pv.read("output.stl")
+     mesh_display.plot()
+
+     mesh =  n.Mesh.load(n.File("output.stl"))
+     segmentcloud = mesh.data
+     areas =  mesh.get_surface_area_per_segment()
+     normals = segmentcloud.get_segment_normals()
+
+    #  print(segmentcloud.points)
+    #  print(segmentcloud.segments)
+    #  print(segmentcloud.point_attributes) #-->empty dict, how to proceed?
+    #  print(segmentcloud.point_attributes["face_normal"])# KeyError: 'face_normal'
+    #  print(segmentcloud.point_attributes["area"])# KeyError: 'area'
+     
+     #Riemann sum of a unit vector
+     dot_product = np.sum(normals, axis=1) #horizontally sum across normals -> 1
+     riemann_sum = np.sum(dot_product * areas)
+    
+     print('Riemann sum is:')
+     print(mesh.get_surface_area())
+     print(riemann_sum)
+
+
+
+
+
+# Step 1: Display particles
+visualize_particles()
+# Step 2: Write CV grid to STL
+write_to_stl()
+
+#Read back into nemo
+#Do a closest neighbour search of particles within each grid element --> I'm not entirely sure how to do this right now 
+read_into_nemo()
